8417d7917caff4d1041b672ed06f781cb93cfde7
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-06-06 18:15:07 +0530

    basic setup completed
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

---
+++ app/routes/root/home.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { Route } from "../../+types/root";
+import { Button } from "@/components/ui/button";
+import { Avatar } from "@/components/ui/avatar";
+import { Link } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Trelloq" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+const Homepage = () => {
+  return (
+    <div>
+      <div className="w-full h-screen flex items-center justify-center gap-4">
+        <Link to="/sign-in">
+          <Button className="bg-blue-500 text-white">Login</Button>
+        </Link>
+        <Link to="/sign-up">
+          <Button variant="outline" className="bg-blue-500 text-white">Sign Up</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
