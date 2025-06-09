06af185036608dc62210d16d5fe810e8e34c7d2f
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-06-09 22:59:21 +0530

    dashboard layout initiated
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- app/routes/dashboard/dashboardLayout.tsx
+++ app/routes/dashboard/dashboardLayout.tsx
@@ -1,11 +1,52 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/layout/header";
+import { SidebarComponent } from "@/components/layout/sidebarComponent";
+import Loader from "@/components/loader";
+import CreateWorkspace from "@/components/workspace/createWorkspace";
 import { useAuth } from "@/provider/authContext";
+import type { Workspace } from "@/types";
+import { useState } from "react";
+import { Navigate, Outlet } from "react-router";
 
 const DashboardLayout = () => {
-  const {user, logout} = useAuth();
-  return <div className="h-screen flex justify-center items-center">
-    <Button onClick={logout}>Logout</Button>
-  </div>;
+  const { isAuthenticated, isLoading } = useAuth();
+  const [isCreatingWorkspace, setIsCreatingWorkspace] = useState(false)
+  const [currentWorkspace, setCurrentWorkspace] = useState<Workspace | null>(null)
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  const handleWorkspaceSelected = (workspace: Workspace) => {
+    setCurrentWorkspace(workspace)
+  }
+
+  return (
+    <div className="h-screen flex w-full">
+      <SidebarComponent currentWorkspace={currentWorkspace}/>
+
+      <div className="flex flex-col h-full flex-1">
+        <Header
+        onWorkspaceSelected={handleWorkspaceSelected}
+        selectedWorkspace={currentWorkspace}
+        onCreateWorkspace={() => setIsCreatingWorkspace(true)}
+        />
+        <main className="flex flex-1 overflow-y-auto h-full w-full">
+          <div className="mx-auto container px-2 sm:px-6 lg:px-8 py-0 md:py-8 w-full h-full">
+          <Outlet />
+          </div>
+        </main>
+      </div>
+
+      <CreateWorkspace
+        isCreatingWorkspace={isCreatingWorkspace}
+        setIsCreatingWorkspace={setIsCreatingWorkspace}
+      />
+    </div>
+  );
 };
 
 export default DashboardLayout;
