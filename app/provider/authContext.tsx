5dcd49b131e46a4fe7e2ff4a23b7b73a1ffc276b
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-06-27 02:53:52 +0530

    fixed small refresh browser bug/added create project frontend
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- app/provider/authContext.tsx
+++ app/provider/authContext.tsx
@@ -17,7 +17,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const navigate = useNavigate();
   const currentPath = useLocation().pathname;
--- app/routes/dashboard/workspaces/workspaceDetails.tsx
+++ app/routes/dashboard/workspaces/workspaceDetails.tsx
@@ -1,9 +1,60 @@
-const WorkspaceDetails = () => {
-  return (
-    <div>
-      Workspace Details
-    </div>
-  )
-}
+import Loader from "@/components/loader";
+import CreateProjectDialog from "@/components/project/createProjectDialog";
+import ProjectList from "@/components/workspace/projectList";
+import WorkspaceHeader from "@/components/workspace/workspaceHeader";
+import { useGetWorkspaceQuery } from "@/hooks/useWorkspace";
+import type { Project, Workspace } from "@/types";
+import { useState } from "react";
+import { useParams } from "react-router";
 
-export default WorkspaceDetails
+const WorkspaceDetails = () => {
+  const { workspaceId } = useParams<{ workspaceId: string }>();
+  const [isCreateProject, setIsCreateProject] = useState(false);
+  const [isInviteMember, setIsInviteMember] = useState(false);
+
+  if (!workspaceId) {
+    return <div>No Workspace Found</div>;
+  }
+
+  const { data, isLoading } = useGetWorkspaceQuery(workspaceId) as {
+    data: {
+      workspace: Workspace;
+      projects: Project[];
+    };
+    isLoading: boolean;
+  };
+
+  if (isLoading) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className="space-y-8">
+      <WorkspaceHeader
+        workspace={data.workspace}
+        members={data?.workspace?.members as any}
+        onCreateProject={() => setIsCreateProject(true)}
+        onInviteMember={() => setIsInviteMember(true)}
+      />
+
+      <ProjectList
+        workspaceId={workspaceId}
+        projects={data.projects}
+        onCreateProject={() => setIsCreateProject(true)}
+      />
+
+      <CreateProjectDialog
+        isOpen={isCreateProject}
+        onOpenChange={setIsCreateProject}
+        workspaceId={workspaceId}
+        workspaceMembers={data.workspace.members as any}
+      />
+    </div>
+  );
+};
+
+export default WorkspaceDetails;
